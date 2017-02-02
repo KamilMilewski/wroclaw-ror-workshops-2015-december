6bbf30d1dfed7da8a76e40bc4388fc2c713babbb
Kamil Milewski <kamil.milewski@hotmail.com>
2017-02-02 18:19:41 +0100

    Implement avg_notes in student decorator

--- app/decorators/student_decorator.rb
+++ app/decorators/student_decorator.rb
@@ -3,5 +3,9 @@ class StudentDecorator < BaseDecorator
     "#{first_name} #{last_name}"
   end
 
-  def avg_notes(subject_item); end
+  def avg_notes(subject_item)
+    return '0.00' if (count = subject_item.subject_item_notes.count).zero?
+    sum = subject_item.subject_item_notes.map(&:value).inject(:+).to_f
+    format '%.2f', sum / count
+  end
 end
