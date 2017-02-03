84a7203bd5eea98be85f436663d431648bd7ad8b
Kamil Milewski <kamil.milewski@hotmail.com>
2017-02-03 10:05:17 +0100

    Update student factory with birthdate

--- spec/factories/students.rb
+++ spec/factories/students.rb
@@ -2,5 +2,6 @@ FactoryGirl.define do
   factory :student do
     first_name { Faker::Name.first_name }
     last_name { Faker::Name.last_name }
+    birthdate { Faker::Date.between(20.years.ago, 15.years.ago) }
   end
 end
