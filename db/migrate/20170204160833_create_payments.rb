81a2d299afb7d5088d9da817da15ebc582bb4dce
Kamil Milewski <kamil.milewski@hotmail.com>
2017-02-04 17:47:05 +0100

    Change references in payment mimgraion
    From user to student because this was an error

--- db/migrate/20170204160833_create_payments.rb
+++ db/migrate/20170204160833_create_payments.rb
@@ -1,7 +1,7 @@
 class CreatePayments < ActiveRecord::Migration
   def change
     create_table :payments do |t|
-      t.references :user, index: true, foreign_key: true
+      t.references :student, index: true, foreign_key: true
       t.integer :month
       t.integer :year
       t.date :paid_at
