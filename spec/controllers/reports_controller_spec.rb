98bcb4c82f77a39301106ed55f698a5e40da564e
Kamil Milewski <kamil.milewski@hotmail.com>
2017-02-04 20:54:59 +0100

    Create user_visits_student_payments_page_spec.rb

--- spec/controllers/reports_controller_spec.rb
+++ spec/controllers/reports_controller_spec.rb
@@ -1,13 +1,35 @@
-require 'rails_helper'
+require 'spec_helper'
 
-RSpec.describe ReportsController do
-  let(:user) { create :user }
+feature 'User visits student payments page' do
+  let!(:student_1) { create :student, first_name: 'Jan', last_name: 'Adam' }
+  let!(:payment_1) do
+    create :payment, student: student_1,
+                     year: 2000,
+                     month: 6,
+                     paid_at: Date.new(2000, 6, 23)
+  end
 
-  before { sign_in user }
+  background do
+    sign_in
+    expect(page).to have_content 'Logout'
+    visit payments_path
+  end
 
-  describe 'GET #subjects' do
-    subject { get :subjects }
+  scenario 'should see students list' do
+    within('.breadcrumbs') do
+      expect(page).to have_content 'RoR Workhops » Payments'
+    end
 
-    it_behaves_like 'template rendering action', :subjects
+    expect(page).to have_content 'Jan'
+    expect(page).to have_content 'Adam'
+    expect(page).to have_content '2000'
+    expect(page).to have_content '6'
+    expect(page).to have_content '2000-06-23'
+  end
+
+  scenario 'only when sign in' do
+    logout
+    visit payments_path
+    expect(page).to have_content 'You need to sign in or sign up before continuing.'
   end
 end
